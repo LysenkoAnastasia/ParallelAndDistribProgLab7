95c3afe86f071f1fd324adb09ec5c4ab05229ffd
LysenkoAnastasia <lysenko2712@mail.ru>
2019-12-13 14:58:54 +0300

    Scripted auto-commit on change (2019-12-13 14:58:54) by gitwatch.sh

--- lab7/src/main/java/Broker.java
+++ lab7/src/main/java/Broker.java
@@ -1,3 +1,11 @@
-public class Router {
+import org.zeromq.SocketType;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+
+public class Broker {
+    ZContext ctx = new ZContext();
+    ZMQ.Socket frontend = ctx.createSocket(SocketType.DEALER);
+    ZMQ.Socket backend = ctx.createSocket(SocketType.DEALER);
     
+
 }
