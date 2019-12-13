4ac9789bbe292e405985d72197878869cad8755d
LysenkoAnastasia <lysenko2712@mail.ru>
2019-12-13 15:24:03 +0300

    Scripted auto-commit on change (2019-12-13 15:24:03) by gitwatch.sh

--- lab7/src/main/java/Broker.java
+++ lab7/src/main/java/Broker.java
@@ -6,13 +6,20 @@ public class Broker implements Runnable {
 
     @Override
     public void run() {
-        ZContext ctx = new ZContext();
-        Socket responder = ctx.createSocket(SocketType.DEALER);
-        responder.connect("tcp://localhost:5560");
+        try {
+            ZContext ctx = new ZContext();
+            Socket responder = ctx.createSocket(SocketType.DEALER);
+            responder.connect("tcp://localhost:5560");
 
-        while (!Thread.currentThread().isInterrupted()) {
-            ZMQ.Poller items = ctx.createPoller(2);
-            items.register(responder, ZMQ.Poller.POLLIN);
+            while (!Thread.currentThread().isInterrupted()) {
+                ZMQ.Poller items = ctx.createPoller(2);
+                items.register(responder, ZMQ.Poller.POLLIN);
+                Thread.sleep(1000);
+
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+
     }
 }
