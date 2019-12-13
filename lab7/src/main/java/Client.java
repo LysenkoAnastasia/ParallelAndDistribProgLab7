b762cc49f64da88b3f06671d0c56e1fbde34faea
LysenkoAnastasia <lysenko2712@mail.ru>
2019-12-13 16:38:43 +0300

    Scripted auto-commit on change (2019-12-13 16:38:43) by gitwatch.sh

--- lab7/src/main/java/Client.java
+++ lab7/src/main/java/Client.java
@@ -1,4 +1,6 @@
+import org.zeromq.SocketType;
 import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
 
 public class Client implements Runnable {
     private static int SAMPLE_SIZE = 10000;
@@ -6,6 +8,10 @@ public class Client implements Runnable {
     @Override
     public void run() {
         ZContext ctx = new ZContext();
-        
+        ZMQ.Socket client = ctx.createSocket(SocketType.DEALER);
+        client.setHWM(0);
+        client.setIdentity("C".getBytes(ZMQ.CHARSET));
+        client.connect("tcp://localhost:5555");
+        System.out.println("Setting up test");
     }
 }
