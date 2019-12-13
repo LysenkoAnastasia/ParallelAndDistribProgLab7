b3a09b94b168225e19f263694b370fd59614a05d
LysenkoAnastasia <lysenko2712@mail.ru>
2019-12-13 16:18:23 +0300

    Scripted auto-commit on change (2019-12-13 16:18:23) by gitwatch.sh

--- lab7/src/main/java/Client.java
+++ lab7/src/main/java/Client.java
@@ -1,8 +1,11 @@
+import org.zeromq.ZContext;
+
 public class Client implements Runnable {
     private static int SAMPLE_SIZE = 10000;
 
     @Override
     public void run() {
-       
+        ZContext ctx = new ZContext();
+        
     }
 }
