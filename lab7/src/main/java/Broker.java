b4772b16408d58e8e82bc8eb065fff433d8e9055
LysenkoAnastasia <lysenko2712@mail.ru>
2019-12-13 15:08:47 +0300

    Scripted auto-commit on change (2019-12-13 15:08:47) by gitwatch.sh

--- lab7/src/main/java/Broker.java
+++ lab7/src/main/java/Broker.java
@@ -11,7 +11,16 @@ public class Broker implements Runnable {
         responder.connect("tcp://localhost:5560");
 
         while (!Thread.currentThread().isInterrupted()) {
-            
+            // Wait for next request from client
+            String string = responder.recvStr (0);
+            System.out.printf ("Received request: [%s]\n", string);
+// Do some 'work'
+            Thread.sleep (1000);
+// Send reply back to client
+            responder.send ("World");
+// We never get here but clean up anyhow
+            responder.close();
+            context.term();
         }
     }
 }
