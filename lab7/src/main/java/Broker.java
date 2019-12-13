3572ba02c21b0c300594df42f1d8f0cbbf54a974
LysenkoAnastasia <lysenko2712@mail.ru>
2019-12-13 16:04:14 +0300

    Scripted auto-commit on change (2019-12-13 16:04:14) by gitwatch.sh

--- lab7/src/main/java/Broker.java
+++ lab7/src/main/java/Broker.java
@@ -35,6 +35,7 @@ public class Broker implements Runnable {
                 if (items.pollin(1)) {
                     while (true) {
                         message = backend.recv(0);
+                        
                         frontend.send(message, more ? ZMQ.SNDMORE : 0);
                         if(!more){
                             break;
