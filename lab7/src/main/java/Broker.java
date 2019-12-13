39cc644c00f10f1d28d91cb716fe0d3ef9d1fa9c
LysenkoAnastasia <lysenko2712@mail.ru>
2019-12-13 15:07:13 +0300

    Scripted auto-commit on change (2019-12-13 15:07:13) by gitwatch.sh

--- lab7/src/main/java/Broker.java
+++ lab7/src/main/java/Broker.java
@@ -3,19 +3,15 @@ import org.zeromq.*;
 import org.zeromq.ZMQ.Socket;
 
 public class Broker implements Runnable {
-  try( ZContext ctx = new ZContext())
-
-    {
-        Socket responder = ctx.createSocket(SocketType.DEALER);
-        responder.connect("tcp://localhost:5560");
-
-        while () {
-
-        }
-    }
 
     @Override
     public void run() {
+         ZContext ctx = new ZContext();
+            Socket responder = ctx.createSocket(SocketType.DEALER);
+            responder.connect("tcp://localhost:5560");
 
+            while () {
+
+            }
     }
 }
