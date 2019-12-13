a9a5a4a29f4a55bfcd381d3fae9a029478608f92
LysenkoAnastasia <lysenko2712@mail.ru>
2019-12-13 22:13:39 +0300

    Scripted auto-commit on change (2019-12-13 22:13:39) by gitwatch.sh

--- lab7/src/main/java/Client.java
+++ lab7/src/main/java/Client.java
@@ -14,11 +14,9 @@ public class Client implements Closeable {
     long start;
 
     public void run() {
-        ZContext context = new ZContext();
-        ZMQ.Socket client = context.createSocket(SocketType.DEALER);
-        client.setHWM(0);
-        client.setIdentity("C".getBytes(ZMQ.CHARSET));
-        client.connect("tcp://localhost:5555");
+        socket.setHWM(0);
+        socket.setIdentity("C".getBytes(ZMQ.CHARSET));
+        socket.connect("tcp://localhost:5555");
         System.out.println("Setting up test");
         try {
             Thread.sleep(1000);
@@ -31,16 +29,16 @@ public class Client implements Closeable {
         for (requests = 0; requests < SAMPLE_SIZE; requests++) {
             ZMsg req = new ZMsg();
             req.addString("client");
-            req.send(client);
+            req.send(socket);
         }
 
         for (requests = 0;
              requests < SAMPLE_SIZE && !Thread.currentThread()
                      .isInterrupted();
              requests++) {
-            ZMsg.recvMsg(client).destroy();
+            ZMsg.recvMsg(socket).destroy();
         }
-        context.destroySocket(client);
+        context.destroySocket(socket);
     }
 
     @Override
