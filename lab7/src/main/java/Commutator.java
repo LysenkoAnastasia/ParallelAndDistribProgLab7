3329919c5b172f4dcaae264508c4225dea9d9f13
LysenkoAnastasia <lysenko2712@mail.ru>
2019-12-18 20:36:13 +0300

    Scripted auto-commit on change (2019-12-18 20:36:13) by gitwatch.sh

--- lab7/src/main/java/Commutator.java
+++ lab7/src/main/java/Commutator.java
@@ -1,3 +1,13 @@
 public class Commutator {
+    private String leftBound;
+    private String rightBound;
+    private long time;
 
+    public String getLeftBound() {
+        return leftBound;
+    }
+
+    public String getRightBound() {
+        return rightBound;
+    }
 }
