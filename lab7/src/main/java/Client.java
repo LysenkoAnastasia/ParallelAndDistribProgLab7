cd22cf2a531dd429ea483faa805b5e7f8592d43c
LysenkoAnastasia <lysenko2712@mail.ru>
2019-12-27 16:48:37 +0300

    Scripted auto-commit on change (2019-12-27 16:48:37) by gitwatch.sh

--- lab7/src/main/java/Client.java
+++ lab7/src/main/java/Client.java
@@ -1,44 +1,59 @@
-import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
 import org.zeromq.*;
 
 import java.util.Scanner;
 
 public class Client{
-    private static int SAMPLE_SIZE = 10000;
-    private  static ZContext context;
-    private  static  ZMQ.Socket socket;
-    private static Scanner in = new Scanner(System.in);
+    private  ZContext context;
+    private ZMQ.Socket socket;
+    private Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("connect");
         try {
-            context = new ZContext();
-            socket = context.createSocket(SocketType.REQ);
-            socket.setHWM(0);
-            socket.connect("tcp://localhost:5555");
-            while (true) {
-
-                String message = in.nextLine();
-                if (message.contains("GET") || message.contains("PUT")) {
-                    ZMsg msg = new ZMsg();
-                    msg.addString(message);
-                    msg.send(socket);
-
-                    ZMsg req = ZMsg.recvMsg(socket);
-                    if (req == null) {
-                        break;
-                    }
-                    String s = req.popString();
-                    System.out.println("IN : " + s);
-                    req.destroy();
-                }
-                else  {
-                    System.out.println("incorrect");
-                }
-            }
+            ZContext context = new ZContext();
+            //System.out.println("BEFORE");
+            Client client = new Client(context);
+            client.connect();
+            client.handle();
 
         } catch (ZMQException ex) {
             ex.printStackTrace();
+        }
+    }
+
+    public Client(ZContext context) {
+        this.context = context;
+        this.socket = context.createSocket(SocketType.REQ);
+        socket.setHWM(0);
+    }
+
+    public void connect() {
+        socket.connect("tcp://localhost:5555");
+    }
+
+    public void handle() {
+        while (true) {
+
+            String message = in.nextLine();
+            if (message.contains("GET") || message.contains("PUT")) {
+                //System.out.println("IN LOOP");
+                ZMsg msg = new ZMsg();
+                msg.addString(message);
+                msg.send(socket);
+
+                ZMsg req = ZMsg.recvMsg(socket);
+                if (req == null) {
+                   // System.out.println("BREAK");
+                    break;
+                }
+                String s = req.popString();
+                System.out.println("IN : " + s);
+                req.destroy();
+            }
+            else  {
+                System.out.println("incorrect");
+            }
         }
     }
 }
